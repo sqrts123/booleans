ec5b24f5fb8d5063fd2e56a6132b2ec04c95d09e
Rhys Wilcoxen <wilcorhy001@warren.k12.in.us>
2024-10-21 09:50:27 -0400

    initial commit

--- maine.py
+++ maine.py
@@ -2,6 +2,6 @@ age = int(input("Insert your age: "))
 out = age > 17
 print(out)
 if out:
-    print("you can vote (false)")
+    print("you can vote (true)")
 else:
     print("You cannot vote (false)")
